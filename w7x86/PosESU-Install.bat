109292281df2c5d67dbad2c7e76987c3f95b8788
togwar <tiagodalua@gmail.com>
2025-01-15 14:44:44 -0300

    Update PosESU-Install.bat

--- w7x86/PosESU-Install.bat
+++ w7x86/PosESU-Install.bat
@@ -3,5 +3,5 @@ wusa.exe "12 - (Optional) KB5013637/windows6.1-kb5013637-x86_38e65611a167a7acb7c
 wusa.exe "12 - (Optional) KB5020861/windows6.1-kb5020861-x86_475b218e81e20dedb69884e7c00d0a8bf36cebf9.msu" /quiet /forcerestart
 wusa.exe "13 - (Optional) KB5017397/windows6.1-kb5017397-x86_96b91eb53575a201d59b1a2b540aa15df0d23b3a.msu" /quiet /forcerestart
 wusa.exe "14 - (Optional) KB4578952/windows6.1-kb4578952-x86_7246565d7fec14ad0381deac93faef3024068eb6.msu" /quiet /forcerestart
-wusa.exe "15 - (Optional) KB5010798/windows6.1-kb5022338-x86_490dce532d299588e11abf3790ff1600482525cd.msu" /quiet /forcerestart
-wusa.exe "15 - (Optional) KB5022338/windows6.1-kb5010798-x86_3c5d6092c9c3f20f3ae0333fa8f5ed12298c4e6a.msu" /quiet /forcerestart
+wusa.exe "15 - (Optional) kb5010798/windows6.1-kb5010798-x86_3c5d6092c9c3f20f3ae0333fa8f5ed12298c4e6a.msu" /quiet /forcerestart
+wusa.exe "15 - (Optional) KB5022338/windows6.1-kb5022338-x86_490dce532d299588e11abf3790ff1600482525cd.msu" /quiet /forcerestart
