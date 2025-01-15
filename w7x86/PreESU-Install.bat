66dc4bf0cac1b4573f03976bce0c9872e7800c63
togwar <tiagodalua@gmail.com>
2025-01-15 13:27:32 -0300

    Update PreESU-Install.bat

--- w7x86/PreESU-Install.bat
+++ w7x86/PreESU-Install.bat
@@ -2,7 +2,7 @@ wusa.exe "02 - Root Certificate Update (x86)/Windows6.1-KB3004394-v2-x86.msu" /q
 wusa.exe "03 - Servicing Stack Update - March 2019 (x86)/windows6.1-kb4490628-x86_3cdb3df55b9cd7ef7fcb24fc4e237ea287ad0992.msu" /quiet /forcerestart
 wusa.exe "04 - SHA-2 Code Signing Support Update - September 2019 (x86)/windows6.1-kb4474419-v3-x86_0f687d50402790f340087c576886501b3223bec6.msu" /quiet /forcerestart
 wusa.exe "05 - Servicing Stack Update (x86)/windows6.1-kb4536952-x86_f3b49481187651f64f13a0369c86ad7caa83b190.msu" /quiet /forcerestart
-wusa.exe "06 - Windows Update Agent (x86)/WindowsUpdateAgent-7.6-x86.exe" /quiet /forcerestart
+wusa.exe "06 - Windows Update Agent (x86)/WindowsUpdateAgent-7.6-x86.exe" /quiet
 wusa.exe "07 - Speedup Patches - Update Rollup (x86)/windows6.1-kb3179573-x86_e972000ff6074d1b0530d1912d5f3c7d1b057c4a.msu" /quiet /forcerestart
 wusa.exe "08 - Latest Extended Servicing Stack Update - May 2020 (x86)/windows6.1-kb4555449-x86_36683b4af68408ed268246ee3e89772665572471.msu" /quiet /forcerestart
 wusa.exe "09 - ESU Licensing Preparation Package (x86)/windows6.1-kb4575903-x86_5905c774f806205b5d25b04523bb716e1966306d.msu" /quiet /forcerestart
