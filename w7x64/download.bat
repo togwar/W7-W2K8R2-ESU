a1d120a68ab1d1780dc949eef817d8a6ebe299cb
togwar <tiagodalua@gmail.com>
2025-01-15 14:40:08 -0300

    Update download.bat

--- w7x64/download.bat
+++ w7x64/download.bat
@@ -12,5 +12,5 @@ wget.exe "https://catalog.s.download.windowsupdate.com/c/msdownload/update/softw
 wget.exe "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2022/11/windows6.1-kb5020861-x64_9df527e79d8854a4ed1b8fe26c2a66bca7d6b8da.msu" -P "12 - (Optional) KB5020861"
 wget.exe "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/secu/2022/09/windows6.1-kb5017397-x64_2a9999bd20cb964869c59bb16841a76e14030a29.msu" -P "13 - (Optional) KB5017397"
 wget.exe "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2020/10/windows6.1-kb4578952-x64_30ac0df8554c2647017f3b36cc02e833a3187364.msu" -P "14 - (Optional) KB4578952"
-wget.exe "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2023/01/windows6.1-kb5022338-x64_75d100c03bcaee4b62d08004cc382337ed09d327.msu" -P "15 - (Optional) KB5022338"
 wget.exe "https://catalog.s.download.windowsupdate.com/c/msdownload/update/software/updt/2022/01/windows6.1-kb5010798-x64_6f690ddb42ab85d3dcac7e6b34e16eb70df6e477.msu" -P "15 - (Optional) KB5010798"
+wget.exe "https://catalog.s.download.windowsupdate.com/d/msdownload/update/software/secu/2023/01/windows6.1-kb5022338-x64_75d100c03bcaee4b62d08004cc382337ed09d327.msu" -P "15 - (Optional) KB5022338"
