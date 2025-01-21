ee4c73bfe6f4355e9923e82f3db7860cd7fe8119
togwar <tiagodalua@gmail.com>
2025-01-21 15:34:49 -0300

    Update PosESU-InstallAndDelete.bat

--- w7x64/PosESU-InstallAndDelete.bat
+++ w7x64/PosESU-InstallAndDelete.bat
@@ -1,8 +1,15 @@
 
 wusa.exe "11 - (Optional) KB3108210\Windows6.1-KB3102810-x64.msu" /quiet /forcerestart
+rmdir "11 - (Optional) KB3108210" /s /q
 wusa.exe "12 - (Optional) KB5013637\windows6.1-kb5013637-x64_9adc2154ff84511c2dd3aeebab9594999b5c7297.msu" /quiet /forcerestart
+rmdir "12 - (Optional) KB5013637" /s /q
 wusa.exe "12 - (Optional) KB5020861\windows6.1-kb5020861-x64_9df527e79d8854a4ed1b8fe26c2a66bca7d6b8da.msu" /quiet /forcerestart
+rmdir "12 - (Optional) KB5020861" /s /q
 wusa.exe "13 - (Optional) KB5017397\windows6.1-kb5017397-x64_2a9999bd20cb964869c59bb16841a76e14030a29.msu" /quiet /forcerestart
+rmdir "13 - (Optional) KB5017397" /s /q
 wusa.exe "14 - (Optional) KB4578952\windows6.1-kb4578952-x64_30ac0df8554c2647017f3b36cc02e833a3187364.msu" /quiet /forcerestart
+rmdir "14 - (Optional) KB4578952" /s /q
 wusa.exe "15 - (Optional) KB5010798\windows6.1-kb5010798-x64_6f690ddb42ab85d3dcac7e6b34e16eb70df6e477.msu" /quiet /forcerestart
+rmdir "15 - (Optional) KB5010798" /s /q
 wusa.exe "15 - (Optional) KB5022338\windows6.1-kb5022338-x64_75d100c03bcaee4b62d08004cc382337ed09d327.msu" /quiet /forcerestart
+rmdir "15 - (Optional) KB5022338" /s /q
