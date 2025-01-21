5cf1ffb37837e95ef29a955e1ea42ea5110f1d4c
togwar <tiagodalua@gmail.com>
2025-01-21 15:53:09 -0300

    Add files via upload

---
+++ w7x86/PosESU-FastInstallAndDelete.bat
@@ -0,0 +1,14 @@
+wusa.exe "11 - (Optional) KB3108210/Windows6.1-KB3102810-x86.msu" /quiet /norestart
+rmdir "11 - (Optional) KB3108210" /s /q
+wusa.exe "12 - (Optional) KB5013637/windows6.1-kb5013637-x86_38e65611a167a7acb7c692e351b95d374ee75579.msu" /quiet /norestart
+rmdir "12 - (Optional) KB5013637" /s /q
+wusa.exe "12 - (Optional) KB5020861/windows6.1-kb5020861-x86_475b218e81e20dedb69884e7c00d0a8bf36cebf9.msu" /quiet /norestart
+rmdir "12 - (Optional) KB5020861" /s /q
+wusa.exe "13 - (Optional) KB5017397/windows6.1-kb5017397-x86_96b91eb53575a201d59b1a2b540aa15df0d23b3a.msu" /quiet /norestart
+rmdir "13 - (Optional) KB5017397" /s /q
+wusa.exe "14 - (Optional) KB4578952/windows6.1-kb4578952-x86_7246565d7fec14ad0381deac93faef3024068eb6.msu" /quiet /norestart
+rmdir "14 - (Optional) KB4578952" /s /q
+wusa.exe "15 - (Optional) kb5010798/windows6.1-kb5010798-x86_3c5d6092c9c3f20f3ae0333fa8f5ed12298c4e6a.msu" /quiet /norestart
+rmdir "15 - (Optional) kb5010798" /s /q
+wusa.exe "15 - (Optional) KB5022338/windows6.1-kb5022338-x86_490dce532d299588e11abf3790ff1600482525cd.msu" /quiet /forcerestart
+rmdir "15 - (Optional) KB5022338" /s /q
