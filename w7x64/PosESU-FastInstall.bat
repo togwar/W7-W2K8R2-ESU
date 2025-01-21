bb7e579c70d54a1841de98c90a8e8c21adcf7ecc
togwar <tiagodalua@gmail.com>
2025-01-21 18:20:56 -0300

    Update PosESU-FastInstall.bat

--- w7x64/PosESU-FastInstall.bat
+++ w7x64/PosESU-FastInstall.bat
@@ -1,7 +1,7 @@
-wusa.exe "11 - (Optional) KB3108210\Windows6.1-KB3102810-x64.msu" /quiet /norestart
-wusa.exe "12 - (Optional) KB5013637\windows6.1-kb5013637-x64_9adc2154ff84511c2dd3aeebab9594999b5c7297.msu" /quiet /norestart
-wusa.exe "12 - (Optional) KB5020861\windows6.1-kb5020861-x64_9df527e79d8854a4ed1b8fe26c2a66bca7d6b8da.msu" /quiet /norestart
-wusa.exe "13 - (Optional) KB5017397\windows6.1-kb5017397-x64_2a9999bd20cb964869c59bb16841a76e14030a29.msu" /quiet /norestart
-wusa.exe "14 - (Optional) KB4578952\windows6.1-kb4578952-x64_30ac0df8554c2647017f3b36cc02e833a3187364.msu" /quiet /norestart
-wusa.exe "15 - (Optional) KB5010798\windows6.1-kb5010798-x64_6f690ddb42ab85d3dcac7e6b34e16eb70df6e477.msu" /quiet /norestart
-wusa.exe "15 - (Optional) KB5022338\windows6.1-kb5022338-x64_75d100c03bcaee4b62d08004cc382337ed09d327.msu" /quiet /norestart
+wusa.exe "11 - POS ESU (Optional)\Windows6.1-KB3102810-x64.msu" /quiet /norestart
+wusa.exe "11 - POS ESU (Optional)\windows6.1-kb4578952-x64_30ac0df8554c2647017f3b36cc02e833a3187364.msu" /quiet /norestart
+wusa.exe "11 - POS ESU (Optional)\windows6.1-kb5010798-x64_6f690ddb42ab85d3dcac7e6b34e16eb70df6e477.msu" /quiet /norestart
+wusa.exe "11 - POS ESU (Optional)\windows6.1-kb5013637-x64_9adc2154ff84511c2dd3aeebab9594999b5c7297.msu" /quiet /norestart
+wusa.exe "11 - POS ESU (Optional)\windows6.1-kb5017397-x64_2a9999bd20cb964869c59bb16841a76e14030a29.msu" /quiet /norestart
+wusa.exe "11 - POS ESU (Optional)\windows6.1-kb5020861-x64_9df527e79d8854a4ed1b8fe26c2a66bca7d6b8da.msu" /quiet /norestart
+wusa.exe "11 - POS ESU (Optional)\windows6.1-kb5022338-x64_75d100c03bcaee4b62d08004cc382337ed09d327.msu" /quiet /norestart
