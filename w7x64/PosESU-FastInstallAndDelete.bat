e3294e9ebcffd2133a711869740271973e489929
togwar <tiagodalua@gmail.com>
2025-01-24 11:03:57 -0300

    Update PosESU-FastInstallAndDelete.bat

--- w7x64/PosESU-FastInstallAndDelete.bat
+++ w7x64/PosESU-FastInstallAndDelete.bat
@@ -1,5 +1,7 @@
 wusa.exe "11 - POS ESU (Optional)\Windows6.1-KB3102810-x64.msu" /quiet /norestart
 del "11 - POS ESU (Optional)\Windows6.1-KB3102810-x64.msu" /s /q
+wusa.exe "11 - POS ESU (Optional)\windows6.1-kb5006743-x64_fb16fb35510d187cd7cf879684d6a125b5e3bb35.msu" /quiet /norestart
+del "11 - POS ESU (Optional)\windows6.1-kb5006743-x64_fb16fb35510d187cd7cf879684d6a125b5e3bb35.msu" /s /q
 wusa.exe "11 - POS ESU (Optional)\windows6.1-kb5013637-x64_9adc2154ff84511c2dd3aeebab9594999b5c7297.msu" /quiet /norestart
 del "11 - POS ESU (Optional)\windows6.1-kb5013637-x64_9adc2154ff84511c2dd3aeebab9594999b5c7297.msu" /s /q
 wusa.exe "11 - POS ESU (Optional)\windows6.1-kb5020861-x64_9df527e79d8854a4ed1b8fe26c2a66bca7d6b8da.msu" /quiet /norestart
